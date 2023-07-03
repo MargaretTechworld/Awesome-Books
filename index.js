c949e08d690e7b28b79a7e6b3ee75b157d824b5d
MargaretTechworld <margarettechworld@gmail.com>
2023-07-03 20:20:58 +0000

    Add Awesome Books JavaScript file

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-let booksInfo=[];
+
+let buttonAdd = document.querySelector(".add-button");
+let bookTitle = document.querySelector(".input-field1");
+let bookAuthor = document.querySelector(".input-field2");
+let bookShelf = document.querySelector(".books-section");
+
+let awesomeBooks = [];
+
+function addAwesomeBooks(){
+  awesomeBooks.push({title: bookTitle.value, author: bookAuthor.value});
+}
+
+function clearInput(){
+  bookTitle.value="";
+  bookAuthor.value="";
+}
+function savedBooks(){
+  window.localStorage.setItem("books",JSON.stringify(awesomeBooks));
+}
+
+
+
+
+buttonAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  addAwesomeBooks();
+  savedBooks();
+  clearInput();
+  console.log(awesomeBooks)
+});
+
+function showBooks(){
+  const bookStored = JSON.parse(window.localStorage.getItem(awesomeBooks));
+  let displayBooks = bookStored.map(
+    (book) => `
+  <p class="book-title">${addAwesomeBooks.title}</p>
+  <p class="book-author">${addAwesomeBooks.author}</p>
+  <button class="remove-button">Remove</button>
+  <hr />
+  `, );
+  bookShelf.innerHTML= displayBooks.join("");
+  }
+  window.addEventListener("DOMContentLoaded", () => {
+    showBooks();
+  });
